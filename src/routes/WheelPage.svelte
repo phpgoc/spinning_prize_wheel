bff702379cdac536c43f73fc7c8743e538391ce1
zxq <phpgoc@icloud.com>
2026-07-26 02:59:19 +0800

    feat: 增加对战全屏快捷键

--- src/routes/WheelPage.svelte
+++ src/routes/WheelPage.svelte
@@ -2763,7 +2763,7 @@
         <section class="shortcut-group shortcut-battle-area">
           <h3>对战区</h3>
           <div class="shortcut-list sidebar-shortcut-list">
-            <div><span>进入 / 返回全屏</span><kbd>F</kbd></div>
+            <div><span>进入 / 返回全屏</span><kbd>F / H</kbd></div>
             <div><span>微调比分框上 / 下</span><kbd>U / J</kbd></div>
             <div><span>微调比分框左 / 右</span><kbd>H / K</kbd></div>
             <div><span>聚焦单败未完成比分</span><kbd>S</kbd></div>
